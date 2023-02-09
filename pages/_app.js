bf1a927f23988157680cfc4552f92c6994397d76
pzajacz <pzajacz@brainsum.com>
2023-02-09 14:42:07 +0100

    Enable GTM module fix.

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,16 @@ import '../styles/globals.css'
 import {AppWrapper} from "../context/states";
 import { Analytics } from '@vercel/analytics/react';
 import TagManager from "react-gtm-module";
+import {useEffect} from "react";
 
 const tagManagerArgs = {
   gtmId: 'GTM-TL2GXV6'
 }
 
-TagManager.initialize(tagManagerArgs);
-
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    TagManager.initialize(tagManagerArgs);
+  },[])
   return (
     <AppWrapper>
       <Component {...pageProps} />
